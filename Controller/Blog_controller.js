4758aacaa4420555c07e3df3bb0e259ebec372ac
Mohamed Fazil Z <mdfazilhussain@outlook.com>
2024-09-21 10:01:59 +0530

    image update issue rectified

--- Controller/Blog_controller.js
+++ Controller/Blog_controller.js
@@ -5,34 +5,6 @@ const multer = require("multer");
 const BlogModel = require("../Model/Blog_model.js");
 const upload = require("../Middleware/multer_config.js");
 
-// Configure multer for image upload
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "/blog_images"); // Define your uploads folder
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, Date.now() + path.extname(file.originalname)); // Rename file with timestamp
-//   },
-// });
-
-// const upload = multer({
-//   storage: storage,
-//   limits: { fileSize: 1024 * 1024 * 5 }, // Limit file size to 5MB
-//   fileFilter: function (req, file, cb) {
-//     const fileTypes = /jpeg|jpg|png/;
-//     const extname = fileTypes.test(
-//       path.extname(file.originalname).toLowerCase()
-//     );
-//     const mimeType = fileTypes.test(file.mimetype);
-
-//     if (extname && mimeType) {
-//       return cb(null, true);
-//     } else {
-//       cb(new Error("Images only! (jpeg, jpg, png)"));
-//     }
-//   },
-// });
-
 // Helper function to delete old image
 const deleteOldImage = (imagePath) => {
   fs.unlink(imagePath, (err) => {
@@ -44,29 +16,24 @@ const deleteOldImage = (imagePath) => {
 
 // CREATE BLOG
 const createBlog = async (req, res) => {
-  // upload(req, res, async function (err) {
-  //   if (err instanceof multer.MulterError || err) {
-  //     return res.status(400).json({ status: false, message: err.message });
-  //   }
-
-    try {
-      const { title, content, author } = req.body;
-      const result = await BlogModel.createBlog(
-        title,
-        content,
-        req.file.filename,
-        author
-      );
-      console.log("Blog created successfully");
-      res
-        .status(201)
-        .json({ status: true, message: "Blog created successfully" });
-    } catch (error) {
-      res
-        .status(500)
-        .json({ status: false, message: "Internal server error", error });
-    }
-  };
+  try {
+    const { title, content, author } = req.body;
+    const result = await BlogModel.createBlog(
+      title,
+      content,
+      req.file.filename,
+      author
+    );
+    console.log("Blog created successfully");
+    res
+      .status(201)
+      .json({ status: true, message: "Blog created successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ status: false, message: "Internal server error", error });
+  }
+};
 // };
 
 // GET ALL BLOGS
@@ -99,55 +66,53 @@ const getBlogById = async (req, res) => {
 
 // UPDATE BLOG
 const updateBlog = async (req, res) => {
-  // upload(req, res, async function (err) {
-  //   if (err instanceof multer.MulterError || err) {
-  //     return res.status(400).json({ status: false, message: err.message });
-  //   }
-    try {
-      const { id, title, body, author } = req.body;
-      const newImage = req.file ? req.file.filename : null;
-
-      // Fetch the old blog to get the current image
-      const blog = await BlogModel.getBlogById(id);
-      if (blog.length === 0) {
-        return res
-          .status(404)
-          .json({ status: false, message: "Blog not found" });
-      }
-
-      // Delete the old image if a new image is uploaded
-      if (newImage && blog[0].image) {
-        const oldImagePath = path.join(__dirname, "../blog_images", blog[0].image);
-        deleteOldImage(oldImagePath);
-      }
-
-      const result = await BlogModel.updateBlog(
-        id,
-        title,
-        body,
-        newImage || blog[0].image,
-        author
-      );
-      if (result.affectedRows === 0) {
-        return res
-          .status(404)
-          .json({ status: false, message: "Blog not found" });
-      }
-
-      res
-        .status(200)
-        .json({ status: true, message: "Blog updated successfully" });
-    } catch (error) {
-      res
-        .status(500)
-        .json({ status: false, message: "Internal server error", error });
+  try {
+    const { id, title, body, author } = req.body;
+    const newImage = req.file ? req.file.filename : null;
+    // Fetch the old blog to get the current image
+    const blog = await BlogModel.getBlogById(id);
+    if (blog.length === 0) {
+      return res.status(404).json({ status: false, message: "Blog not found" });
     }
-  // });
+
+    // console.log(blog);
+    // Delete the old image if a new image is uploaded
+    if (newImage && blog[0].blog_image) {
+      console.log("image");
+      const oldImagePath = path.join(
+        __dirname,
+        "../blog_images/",
+        blog[0].blog_image
+      );
+      console.log(oldImagePath);
+      deleteOldImage(oldImagePath);
+    }
+
+    const result = await BlogModel.updateBlog(
+      id,
+      title,
+      body,
+      newImage || blog[0].image,
+      author
+    );
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ status: false, message: "Blog not found" });
+    }
+
+    res
+      .status(200)
+      .json({ status: true, message: "Blog updated successfully" });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ status: false, message: "Internal server error", error });
+  }
 };
 
 // DELETE BLOG
 const deleteBlog = async (req, res) => {
-  // try {
+  try {
     const id = req.params.id;
     const blog = await BlogModel.getBlogById(id);
     if (blog.length === 0) {
@@ -155,8 +120,12 @@ const deleteBlog = async (req, res) => {
     }
 
     // Delete the blog image from the folder
-    if (blog[0].image) {
-      const imagePath = path.join(__dirname, "../blog_images", blog[0].image);
+    if (blog[0].blog_image) {
+      const imagePath = path.join(
+        __dirname,
+        "../blog_images/",
+        blog[0].blog_image
+      );
       deleteOldImage(imagePath);
     }
 
@@ -168,13 +137,12 @@ const deleteBlog = async (req, res) => {
     res
       .status(200)
       .json({ status: true, message: "Blog deleted successfully" });
-  // } catch (error) {
-  //   res
-  //     .status(500)
-  //     .json({ status: false, message: "Internal server error", error });
-  // }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ status: false, message: "Internal server error", error });
+  }
 };
-
 
 module.exports = {
   createBlog,
